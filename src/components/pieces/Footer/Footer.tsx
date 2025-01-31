08f2de1b4617abd9e15d80508267654e1326958f
ALAN-MS-SU <Origin28baker@outlook.com>
2025-01-31 18:13:41 -0300

    footer responsive

--- src/components/pieces/Footer/Footer.tsx
+++ src/components/pieces/Footer/Footer.tsx
@@ -3,10 +3,10 @@ import { FaSquareGithub } from "react-icons/fa6";
 import Link from "next/link";
 export async function Footer() {
   return (
-    <footer className="bg-[var(--primary-light)] h-[200px] opacity-100 border-t-[2px] border-solid border-[var(--secound-light)]  p-10 flex flex-row justify-center gap-[50%] items-center w-[100%]">
-      <h1 className="font-bold">Feito por: ALAN-MS-SU 2025</h1>
+    <footer className="bg-[var(--primary-light)] h-[200px] opacity-100 border-t-[2px] border-solid border-[var(--secound-light)] p-10 flex flex-row justify-center items-center w-[100%] gap-[30%] sm:gap-[35%] md:gap-[40%] lg:gap-[50%]">
+      <h1 className="md:text-[100%] text-[90%] text-nowrap font-bold">Feito por: ALAN-MS-SU 2025</h1>
       <Link href={"https://github.com/ALAN-MS-SU"}>
-        <FaSquareGithub className=" rounded-2xl text-[400%] text-[var(--secound)] transition-all ease-in-out duration-300 hover:text-[600%]" />
+        <FaSquareGithub className="rounded-2xl text-[400%] text-[var(--secound)] transition-all ease-in-out duration-300 hover:text-[600%]" />
       </Link>
     </footer>
   );
