4be755a1bcde0ee2f2f0d72ae432ef3d56b63396
ALAN-MS-SU <origin28baker@outlook.com>
2025-02-16 17:35:30 -0300

    add ListUsers and ConfiUser new fail test

--- test/EditUser/ConfigUser.test.ts
+++ test/EditUser/ConfigUser.test.ts
@@ -16,4 +16,25 @@ describe("Testing api/ConfigUser/:ID", () => {
     });
     expect(response.ok).toBe(true);
   });
+      it("Testing user config failed (no cookie)", async () => {
+        const body = new FormData();
+        const user = new User(1, "Name", Position.USER, "Email", "Password");
+        Object.keys(user).forEach((key) => {
+          body.append(key.toLowerCase(), user[key]);
+        });
+        const response = await fetch(`http://localhost:3000/api/ConfigUser/0`, {
+          method: "PUT",
+          body,
+        });
+        expect(response.ok).toBe(false);
+      });
+  it("Testing user config failed (no user data)", async () => {
+    const response = await fetch(`http://localhost:3000/api/ConfigUser/0`, {
+      method: "PUT",
+      headers: {
+        Cookie: "next-auth.session-token=test",
+      },
+    });
+    expect(response.ok).toBe(false);
+  });
 });
--- test/ListUsers/ListUsers.test.ts
+++ test/ListUsers/ListUsers.test.ts
@@ -1,8 +1,14 @@
 describe("Testing api/ListUsers/:ID", () => {
   it("Testing ListUsers success", async () => {
-    const response = await fetch(`http://localhost:3000/api/ListUsers/1`, {
+    const response = await fetch(`http://localhost:3000/api/ListUsers/0`, {
       method: "GET",
     });
     return expect(response.ok).toBe(true);
   });
+  it("Testing ListUsers failed", async () => {
+    const response = await fetch(`http://localhost:3000/api/ListUsers/fail`, {
+      method: "GET",
+    });
+    return expect(response.ok).toBe(false);
+  })
 });
